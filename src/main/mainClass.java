a07aaf5d83a39a4a1f9246bc988183c9996585ff
yhj <tntj123@naver.com>
2021-09-03 19:45:02 +0900

    현준 완료

--- src/main/mainClass.java
+++ src/main/mainClass.java
@@ -1,5 +1,41 @@
 package main;
 
-public class mainClass {
+import java.util.ArrayList;
+import java.util.Scanner;
 
+import dto.MemDTO;
+import hj.Db;
+
+public class mainClass {
+	public static void main(String[] args) {
+		Scanner scan = new Scanner(System.in);
+		ArrayList<MemDTO>list = new ArrayList<MemDTO>();
+		String id, name, address, gender, info;
+		int age, num, sel;
+		Db hj = new Db();
+
+		System.out.println("---멤버관리 프로그램---");
+		while (true) {
+			System.out.println("1.회원추가 2.회원전체보기 3.회원검색 4.회원수정 5.회원삭제 6.공지사항 발표 및 보기");
+			sel = scan.nextInt();
+			switch (sel) {
+			case 1:
+				break;
+			case 2:
+				//회원 전체보기 기능 추가
+				System.out.println("전체 회원을 보여줍니다.");
+				list = hj.findAll();
+				hj.show(list);
+				break;
+			case 3:
+				break;
+			case 4:
+				break;
+			case 5:
+				break;
+			case 6:
+				break;
+			}
+		}
+	}
 }
