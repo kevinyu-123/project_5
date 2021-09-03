dd62148e5a6ca385f6db62d63d22f9a4d3502bc7
HanGilyong <shkg13@naver.com>
2021-09-03 19:48:31 +0900

    Member Add function 1st

--- src/main/mainClass.java
+++ src/main/mainClass.java
@@ -1,5 +1,26 @@
 package main;
 
-public class mainClass {
+import java.util.Scanner;
 
+import han.addMembership;
+
+public class mainClass {
+	public static void main(String[] args) {
+		Scanner input = new Scanner(System.in);
+		int num=0;
+		addMembership add = new addMembership();
+		boolean bool = true;
+		while(bool) {
+			System.out.println("회원 관리 프로그램입니다.");
+			System.out.println("1.추가 2.전체보기 3.특정보기 4.수정 5.삭제");
+			num = input.nextInt();
+			switch(num) {
+			case 1 : add.display(); break;
+			case 2 : break;
+			case 3 : break;
+			case 4 : break;
+			case 5 : break;
+			}
+		}
+	}
 }
