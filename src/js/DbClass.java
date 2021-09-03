f086ea55156a50747ecb30bc04fa87ce317e9a75
kimjinsu <soosos11234@gmail.com>
2021-09-04 01:52:33 +0900

    으악

--- src/js/DbClass.java
+++ src/js/DbClass.java
@@ -22,57 +22,30 @@ public class DbClass {
 			e.printStackTrace();
 		}
 	}
-		public ArrayList<MemDTO> getList(){
+		public ArrayList<MemDTO> find(String id) {
 			ArrayList<MemDTO> list = new ArrayList<MemDTO>();
-		
-		try {
-			Connection con = DriverManager.getConnection(url, id, pwd);
-			System.out.println("연결됨!?");
-		
-			String sql = "select * from memMangage";
-			PreparedStatement ps = con.prepareStatement(sql);
-		
-			ResultSet rs = ps.executeQuery();
-			
-			while(rs.next()) {
-				MemDTO mem = new MemDTO();
-				mem.setId(rs.getString("id"));
-				mem.setName(rs.getString("name"));
-				mem.setAge(rs.getInt("age"));
-				mem.setGender(rs.getString("gender"));
-				mem.setNum(rs.getString("num"));
-				mem.setAddress(rs.getNString("address"));
-			}
-		}catch (Exception e) {
-				e.printStackTrace();
-		}
-			return list;
-		}
-		
-		public MemDTO searchO(String id) {
-			String sql = "select * from memMangage where id = '"+id+"'";
-			
 			MemDTO dto = null;
+			String sql = "select * from memMangage where id = '"+id+"'";
 			try {
 				Connection con = DriverManager.getConnection(url,this.id,pwd);
-				System.out.println("연결확인");
+				//System.out.println("연결확인");
 				PreparedStatement ps = con.prepareStatement(sql);
-				
 				ResultSet rs = ps.executeQuery();
 				if(rs.next()) {
 					dto = new MemDTO();
-					
 					dto.setId(rs.getString("id"));
 					dto.setName(rs.getString("name"));
 					dto.setAge(rs.getInt("age"));
 					dto.setGender(rs.getString("gender"));
 					dto.setNum(rs.getString("num"));
 					dto.setAddress(rs.getNString("address"));
+					dto.setInfo(rs.getString("info"));
+					list.add(dto);
 				}
 			} catch (Exception e) {
-				// TODO: handle exception
+				e.printStackTrace();
 			}
-			return dto;
+			return list;
 			
 		}
 }
